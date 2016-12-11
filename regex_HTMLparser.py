1022e8eb2683c141dedc41d50b39cc679965149d
Bhupesh Gupta <bhupeshgupta@Bhupeshs-MacBook-Air.local>
2016-12-11 12:28:23 +0530

    first commit

--- regex_HTMLparser.py
+++ regex_HTMLparser.py
@@ -11,3 +11,62 @@ Input = Interger N and N lines of HTML code
 Output = start, end, attribute followed by tag value
 
 '''
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        for attr in attrs:
+            name, value = attr[0], attr[1]
+            print("->", name, ">", value)
+    def handle_endtag(self, tag):
+        print('End :', tag)
+    def handle_startendtag(self, tag, attrs):
+        print('Empty :', tag)
+        for attr in attrs:
+            name, value = attr[0], attr[1]
+            print("-> ", name, " > ", value)
+
+
+
+# main function 
+N = int(input())
+html_feed = ''
+for _ in range(N):
+    html_feed += input() + '\n'    # concatenate the input string into Html_Code
+
+parser = MyHTMLParser() # feed HTML_CODE to parser and done hmm. 
+parser.feed(html_feed)
+
+'''
+editors solution is pretty similar to mine. with two differences 
+1) rather than using name, value editor used " > ".join(map(str, attr))
+2) parser.close() method is used. 
+i should remember parser.close() method 
+
+Editor's code 
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print 'Start :',tag
+        for attr in attrs:
+                print '->',' > '.join(map(str,attr))
+    def handle_endtag(self, tag):
+        print 'End   :',tag
+    def handle_startendtag(self, tag, attrs):
+        print 'Empty :',tag
+        for attr in attrs:
+                print '->',' > '.join(map(str,attr))
+
+html = ""
+for i in range(int(raw_input())):
+    html += raw_input()
+                    
+                
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
+
+
+'''
