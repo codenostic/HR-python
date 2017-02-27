9e551f2bc26f0a707498bd4dba7b6c53c60c3c8b
Bhupesh Gupta <bhupeshgupta@Bhupeshs-MacBook-Air.local>
2017-02-27 11:51:17 +0530

    first commit

--- parse_XML_score.py
+++ parse_XML_score.py
@@ -13,7 +13,16 @@ import sys
 import xml.etree.ElementTree as etree
 
 def get_attr_number(node):
-    pass # your code goes here
+    total_attr = 0    
+    for child in node:
+        if len(child) > 1:
+            total_attr += get_attr_number(child)
+        else:
+            total_attr += len(child.attrib)
+    
+    return total_attr + len(node.attrib)
+
+
 
 
 
@@ -21,5 +30,18 @@ if __name__ == '__main__':
     sys.stdin.readline()
     xml = sys.stdin.read()
     tree = etree.ElementTree(etree.fromstring(xml))
+    print(tree)
     root = tree.getroot()
+    print(root)
     print(get_attr_number(root))
+    
+'''
+another solution - plain and simple 
+def count_of_attr(root):
+    count = len(root.attrib)
+    for child in root:
+        count += count_of_attr(child)
+    return count
+
+then there are other iteraive solutins using .iter etc
+'''
